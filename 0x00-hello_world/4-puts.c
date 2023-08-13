10bd43c533f488d29ef75e949a44f1b931898e0e
Henry Saya <henrysaya@skiff.com>
2023-08-13 11:55:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
 /*
  * main - Entry point
  *
+ * Description - program that prints exactly
+ * "Programming is like building a multilingual puzzle"
+ *
  * Return: Always 0( Success)
  */
 int main(void)
