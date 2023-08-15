da4d7febec18fbddff2af9ff0cb8406c6c54c12e
Henry Saya <henrysaya@skiff.com>
2023-08-15 12:49:28 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "math.h"
+/**
+ * add - function that adds two integers
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: returns the sum
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
