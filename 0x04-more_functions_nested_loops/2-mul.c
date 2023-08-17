bd7c5e84b90537ef6f844a0e7c7fd450f84b0af9
Henry Saya <henrysaya@skiff.com>
2023-08-17 08:12:39 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: digit to multiply with b
+ * @b: digit multiplied by a
+ *
+ * Return: return (product)
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
