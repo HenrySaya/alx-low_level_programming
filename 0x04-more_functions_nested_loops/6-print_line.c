8f627c690ead474f7d43979a8b88dde458310d07
Henry Saya <henrysaya@skiff.com>
2023-08-17 14:07:07 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,23 @@
 #include "main.h"
+
 /**
- * print_line - function that draws a straight line
- *
+ * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
- * Return: no return
  */
 void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar(10);
-	}
-	else
+		_putchar('\n');
+	} else
 	{
 		int i;
 
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
+
 }
