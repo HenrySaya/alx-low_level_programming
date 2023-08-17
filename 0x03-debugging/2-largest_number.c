efbb4ff2151a5d0ade8f37fcf93430d1b7977eaa
Henry Saya <henrysaya@skiff.com>
2023-08-17 02:03:54 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,27 +10,10 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+        int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (a > c && c > b)
-	{
-		largest - a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (b > c && c > a)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-	return (largest);
+        largest = a > b ? a : b;
+        largets = largest > c ? largest : c;
+
+        return (0);
 }
