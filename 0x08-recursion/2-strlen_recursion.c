fc50a7cc58e8ca8c029126b872a18a1d802f8b75
Henry Saya <henrysaya@skiff.com>
2023-08-30 11:03:46 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ *
+ * @s: character to be checked
+ * Return: return the length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i = _strlen_recursion(s + 1);
+	}
+	return (i);
+}
