ad0dfba0a958e7471dcefc5cd227e760976d9617
Henry Saya <henrysaya@skiff.com>
2023-08-30 11:53:43 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,47 @@
 #include "main.h"
 
-int actual_prime(int n, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- * is_prime_number - says if an integer is a prime number or not
- * @n: number to evaluate
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- * Return: 1 if n is a prime number, 0 if not
+ * Return: 1 if it is, 0 it's not
  */
-int is_prime_number(int n)
+int is_palindrome(char *s)
 {
-	if (n <= 1)
-		return (0);
-	return (actual_prime(n, n - 1));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- * actual_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
  *
- * Return: 1 if n is prime, 0 if not
+ * Return: length of the string
  */
-int actual_prime(int n, int i)
+int _strlen_recursion(char *s)
 {
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
+	if (*s == '\0')
 		return (0);
-	return (actual_prime(n, i - 1));
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -18,3 +18,9 @@ a function that returns the natural square root of a number.
 
 ## 6-is_prime_number.c
 This is a function that returns 1 if the input integer is a prime number, otherwise return 0.
+
+## 100-is_palindrome.c
+ a function that returns 1 if a string is a palindrome and 0 if not.
+
+## 101-wildcmp.c
+a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.
