81c54d00d1af18b5702b359331ca91a0a169a555
Henry Saya <henrysaya@skiff.com>
2023-08-30 10:27:53 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ *
+ * @s: characters to be printed
+ * Return: no return
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s +1);
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
