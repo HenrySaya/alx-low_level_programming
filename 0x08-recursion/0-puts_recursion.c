fa4c8122a76d737f7a70622d2b7c1c8157f9a807
Henry Saya <henrysaya@skiff.com>
2023-09-28 07:22:08 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,10 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '/0')
+	while (*s)
 	{
-		putchar('/n');
-		return;
+		putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	putchar(*s);
-	_puts_recursion(s + 1);
+	putchar('/n');
 }
