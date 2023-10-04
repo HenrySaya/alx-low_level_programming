d304999bdabbfdfddd92a8c12565671b9cc04683
Henry Saya <henrysaya@skiff.com>
2023-10-04 13:35:42 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function to print file name
+ *
+ * @argc: count arguments
+ * @argv: print programs name
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
