14458f03911a6a21cec0d4b35d822033844675bd
Henry Saya <henrysaya@skiff.com>
2023-10-11 00:29:13 +0300

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,8 +20,9 @@ struct dog
 /**
  * dog_t - typdef for struct dog
  */
-typdef struct dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
