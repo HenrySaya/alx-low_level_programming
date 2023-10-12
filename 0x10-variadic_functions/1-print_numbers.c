98c4e0e337e204f5c38d3ec93b746a575e06ae99
Henry Saya <henrysaya@skiff.com>
2023-10-13 01:32:45 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (i < (n - 1))
 		{
-			printf(" %d%s", va_arg(args, int), separator);
+			printf("%d%s", va_arg(args, int), separator);
 		}
 		else
 		{
