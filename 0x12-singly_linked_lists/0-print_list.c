80c0531b8621d9a86b6955bb1e515428109ac7ab
Henry Saya <henrysaya@skiff.com>
2023-10-19 14:59:33 +0300

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head pointer of the linked list
+ * Return: returns count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *temp = h;
+
+	if (temp == NULL)
+		printf("[0] (nil)");
+
+	while (temp != NULL)
+	{
+		printf("[%d] %s\n", temp->len, temp->str);
+		count++;
+		temp = temp->next;
+	}
+
+	return (count);
+}
