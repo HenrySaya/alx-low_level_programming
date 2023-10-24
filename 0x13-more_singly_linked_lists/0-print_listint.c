c28361004547d301226667c10c4d0b3b4ea4249a
Henry Saya <henrysaya@skiff.com>
2023-10-24 09:43:54 +0300

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_listint - function to display all elements
+ *
+ * @h: head point of our node
+ * Return: returns number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *ptr;
+
+	ptr = h;
+
+	if (h == NULL)
+		printf("This is an empty list");
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
