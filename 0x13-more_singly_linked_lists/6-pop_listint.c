eeadb6ab378ed0dc2c9a488ef2c2ffab0c20bf46
Henry Saya <henrysaya@skiff.com>
2023-10-24 20:21:33 +0300

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node
+ *
+ * @head: head ointer of listint_t
+ * Return: returns num
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int popped_num;
+
+	if (!head || !(*head))
+		return (0);
+
+	popped_num = (*head)->n;
+	*head = (*head)->next;
+	free (temp);
+	temp = NULL;
+
+	return (popped_num);
+}
