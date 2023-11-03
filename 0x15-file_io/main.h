ab68e7722d21aa2f9f641e0d8f1ea3bf2d48d59c
Henry Saya <henrysaya@skiff.com>
2023-11-03 17:44:38 +0300

    _putchar.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
