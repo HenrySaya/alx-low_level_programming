00eb3e64fb9059fc73c479abbed3843d34992069
Henry Saya <henrysaya@skiff.com>
2023-12-13 22:24:09 +0300

    created dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - detect uppercase alphabets
+ *
+ * @c: input  character
+ *
+ * Return: (1) for true (0) false
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,7 @@
+0x18. C - Dynamic libraries
+
+## libdynamic.so
+This is the dynamic library we created from the .c files we created.
+
+## main.h
+This is the header file that contains all the prototypes of our c files
